4568d0de572d0bb61b1c520a37e1d47e83090b3c
muazamaliofficial <muazamaliofficial@gmail.com>
2022-06-25 20:43:35 +0500

    Initial commit

---
+++ Assets/Scripts/GameWon.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameWon : MonoBehaviour
+{
+    [SerializeField] Canvas gamewinCanvas;
+
+    private void Start()
+    {
+        gamewinCanvas.enabled = false;
+    }
+
+    private void Update ()
+    {
+        if (GameObject.FindGameObjectsWithTag("enemy").Length == 0)
+        {
+            gamewinCanvas.enabled = true;
+            Time.timeScale = 0;
+            FindObjectOfType<WeaponSwitcher>().enabled = false;
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+        }
+
+    }
+}
